2bafa50b4736a16e313934a5422622181a513b52
naolkidanu <sabelkidanu18@gmail.com>
2022-10-19 13:16:02 -0700

    fix linter error

--- script.js
+++ script.js
@@ -1,32 +1,29 @@
 /** @format */
-
-const submit = document.querySelector('.submit');
 const contact = document.querySelector('.contact-list');
 const hamburger = document.querySelector('.hamburger');
 const navUl = document.querySelector('.header-list');
-const project = document.querySelector('.project');
 const log = document.querySelector('.log');
 
 hamburger.addEventListener('click', () => {
-	hamburger.classList.toggle('active');
-	navUl.classList.toggle('active');
-	log.classList.toggle('active');
+  hamburger.classList.toggle('active');
+  navUl.classList.toggle('active');
+  log.classList.toggle('active');
 });
-document.querySelectorAll(".header-list").forEach(n=>n.addEventListener('click',()=>{
-	hamburger.classList.remove('active');
-	navUl.classList.remove('active');
-	log.classList.remove('active');
-}))
+document.querySelectorAll('.header-list').forEach((n) => n.addEventListener('click', () => {
+  hamburger.classList.remove('active');
+  navUl.classList.remove('active');
+  log.classList.remove('active');
+}));
 
 contact.addEventListener('submit', (event) => {
-	const email = document.getElementById('email').value;
-	const error = document.querySelector('.error');
-	const isLowerCase = (str) => str === str.toLowerCase();
-	if (!isLowerCase(email)) {
-		event.preventDefault();
-		error.innerHTML = 'Email should be in lowercase!';
-		setTimeout(() => {
-			error.innerHTML = '';
-		}, 10000);
-	}
+  const email = document.getElementById('email').value;
+  const error = document.querySelector('.error');
+  const isLowerCase = (str) => str === str.toLowerCase();
+  if (!isLowerCase(email)) {
+    event.preventDefault();
+    error.innerHTML = 'Email should be in lowercase!';
+    setTimeout(() => {
+      error.innerHTML = '';
+    }, 10000);
+  }
 });
